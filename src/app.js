79ed9a17d65d58df548f08bc16739e7f32c72f88
Luis Miguel RC312004 <luismiguelrc312004@gmail.com>
2026-02-03 19:21:19 +0000

    Added Generador de cartas aleatorias

--- src/app.js
+++ src/app.js
@@ -1,11 +1,25 @@
-import "bootstrap";
 import "./style.css";
 
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  let numerosCarta = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+  let simbolos = ["Corazones", "Diamantes", "Picas", "Treboles"];
+  let randomNumerosCarta = Math.floor(Math.random() * numerosCarta.length);
+  let randomSimbolos = Math.floor(Math.random() * simbolos.length);
+  let simboloFinal = simbolos[randomSimbolos];
+    
+  document.getElementById('numeroCarta').innerHTML = numerosCarta[randomNumerosCarta];
+  document.getElementById('carta').className = "";
+  document.getElementById('carta').classList.add("card");
+  document.getElementById('carta').classList.add(simboloCarta(simboloFinal));
+
+}
+  
+function simboloCarta(simbolos) {
+    switch (simbolos) {
+      case "Corazones": return "simbolo-corazon";
+      case "Diamantes": return "simbolo-diamante";
+      case "Picas": return "simbolo-pica";
+      case "Treboles": return "simbolo-trebol";
+    }
+}
+
